4a9c19fca46df01c478c281005408eb880b0a14c
JibrilOs <webxite23@gmail.com>
2019-10-27 01:58:44 +0300

    i added yellow color

--- java.js
+++ java.js
@@ -2,3 +2,4 @@ let he, red, blue, yellow ;
 document.getElementsByClassName("he")[0].style.backgroundColor="orange";
 document.getElementsByClassName("red")[0].style.backgroundColor="orange";
 document.getElementsByClassName("blue")[0].style.backgroundColor="blue";
+document.getElementsByClassName("yellow")[0].style.backgroundColor="yellow"
