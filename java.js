e8582ece22dca9efcf6a4787fce3dfecca839114
JibrilOs <54681761+JibrilOs@users.noreply.github.com>
2019-10-28 14:14:13 +0200

    i deleted this  java code

--- java.js
+++
@@ -1,6 +0,0 @@
-let he, red, blue, yellow ;
-document.getElementsByClassName("he")[0].style.backgroundColor="orange";
-document.getElementsByClassName("red")[0].style.backgroundColor="orange";
-document.getElementsByClassName("blue")[0].style.backgroundColor="blue";
-document.getElementsByClassName("yellow")[0].style.backgroundColor="yellow"
-document.getElementsByClassName("black")[0].style.backgroundColor="black"
