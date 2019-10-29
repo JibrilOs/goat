d67ad106f5f055946499947e029d8d814cb60691
JibrilOs <webxite23@gmail.com>
2019-10-29 02:22:59 +0200

    i added a new script that ask for age to display a message whether a car off or on

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-function sayHello(){
+var age = prompt("What is your age?");
 
-  console.log("it is nice to meat you");
+/*function checkDriverAge() {
+                           
+
+                            if (Number(age) < 18) {
+                              alert("Sorry, you are too yound to drive this car. Powering off");
+                            } else if (Number(age) > 18) {
+                              alert("Powering On. Enjoy the ride!");
+                            } else if (Number(age) === 18) {
+                              alert("Congratulations on your first year of driving. Enjoy the ride!");
+                            }
+}
+checkDriverAge(); *
+
+
+function checkDriverAge(age) {
+  if (age < 18) {
+    alert("sorry,you are too young to drive this car.powering off?")
+  } else if (age > 18) {
+
+    alert("Powering on.Enjoy your ride");
+  } else if (age === 18) {
+
+    alert("Congratulations on your first year of driving .Enjoy your ride");
+  } else {
+    alert("you did not input your age please type your age in numbers");
+  }
+
 
 }
 
-sayHello();
+checkDriverAge(777);
+/
